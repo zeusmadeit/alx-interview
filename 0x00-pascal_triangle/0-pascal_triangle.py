b3cc5e8ebc1a8ad53651d0e01603d1514a514921
Abraham Paul <codexapp44@gmail.com>
2024-07-25 17:06:47 +0100

    trying to fix requirement issue

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,46 +3,46 @@
 Implementing Pascal's Triangle in Python
 """
 
-# def pascal_triangle(n: int):
-#     """
-#     Returns a list of lists of integers
-#     representing the Pascal's triangle of n.
-#     """
-#     matrix = [] # empty matrix to store pascal triangle
-
-#     if n <= 0:
-#         return []
-
-#     for row in range(0, n):
-#         temp = []
-#         for col in range(0, row + 1):
-#             if (col == 0 or col == row ):
-#                 temp.append(1)
-#             else:
-#                 temp.append(matrix[row - 1][col - 1] + matrix[row - 1][col])
-#         matrix.append(temp)
-
-#     return matrix
-
-
 def pascal_triangle(n):
-
+    """
+    Returns a list of lists of integers
+    representing the Pascal's triangle of n.
+    """
     matrix = []
 
     if n <= 0:
-        return matrix     
-    
-    matrix = [[1]] 
+        return matrix
 
-    for i in range(1, n):
-        temp = [1] 
-        for j in range(len(matrix[i - 1]) - 1):
-            curr = matrix[i - 1]
-            temp.append(matrix[i - 1][j] + matrix[i - 1][j + 1])
-        temp.append(1)
+    for row in range(0, n):
+        temp = []
+        for col in range(0, row + 1):
+            if (col == 0 or col == row ):
+                temp.append(1)
+            else:
+                temp.append(matrix[row - 1][col - 1] + matrix[row - 1][col])
         matrix.append(temp)
 
     return matrix
+
+
+# def pascal_triangle(n):
+
+#     matrix = []
+
+#     if n <= 0:
+#         return matrix     
+    
+#     matrix = [[1]] 
+
+#     for i in range(1, n):
+#         temp = [1] 
+#         for j in range(len(matrix[i - 1]) - 1):
+#             curr = matrix[i - 1]
+#             temp.append(matrix[i - 1][j] + matrix[i - 1][j + 1])
+#         temp.append(1)
+#         matrix.append(temp)
+
+#     return matrix
 
 
 # def print_triangle(triangle):
