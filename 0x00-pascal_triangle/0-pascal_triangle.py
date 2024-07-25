941c99590839c59f0b0c26f92355e4ef51e5a363
Abraham Paul <codexapp44@gmail.com>
2024-07-25 16:56:29 +0100

    refactored code to use loops

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,16 +5,15 @@ Implementing Pascal's Triangle in Python
 from typing import List
 
 
-def pascal_triangle(n: int) -> List[List]:
+def pascal_triangle(n: int) -> List:
     """
     Returns a list of lists of integers
     representing the Pascal's triangle of n.
     """
+    matrix = [] # empty matrix to store pascal triangle
+
     if n <= 0:
         return []
-    
-    # empty matrix generated to store pascal triangle
-    matrix = []
 
     for row in range(0, n):
         temp = []
@@ -26,3 +25,14 @@ def pascal_triangle(n: int) -> List[List]:
         matrix.append(temp)
 
     return matrix
+
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+print_triangle(pascal_triangle(5))
