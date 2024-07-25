4c5ccb5df64c4e077a109cd3f6634e01263614ca
Abraham Paul <codexapp44@gmail.com>
2024-07-25 17:08:13 +0100

    trying to fix requirement issues

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -23,34 +23,3 @@ def pascal_triangle(n):
         matrix.append(temp)
 
     return matrix
-
-
-# def pascal_triangle(n):
-
-#     matrix = []
-
-#     if n <= 0:
-#         return matrix     
-    
-#     matrix = [[1]] 
-
-#     for i in range(1, n):
-#         temp = [1] 
-#         for j in range(len(matrix[i - 1]) - 1):
-#             curr = matrix[i - 1]
-#             temp.append(matrix[i - 1][j] + matrix[i - 1][j + 1])
-#         temp.append(1)
-#         matrix.append(temp)
-
-#     return matrix
-
-
-# def print_triangle(triangle):
-#     """
-#     Print the triangle
-#     """
-#     for row in triangle:
-#         print("[{}]".format(",".join([str(x) for x in row])))
-
-
-# print_triangle(pascal_triangle(5))
